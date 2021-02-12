3f41db95a89e4b1f819c8fc6a41036ab29069e86
HO.SLEFEVRE <ho.slefevre@cma-cgm.com>
2021-02-12 15:07:57 +0100

    updated password strength middleware

--- middleware/password-strength.js
+++ middleware/password-strength.js
@@ -1,14 +1,14 @@
-var strongRegex = new RegExp("^(?=.{8,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\\W).*$", "g");
 
 const passwordStrength = (req, res, next) => {
   try {
-    if (!req.body.password.match(strongRegex)) {
-      throw new Error('Le mot de passe doit contenir 8 caractères, des chiffres et des lettres minuscules et majuscules !');
+    const regX = /^(?=.{8,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).*$/g;
+    if (!regX.test(req.body.password)) {
+      throw new Error('Le mot de passe doit contenir au moins 8 caractères, des chiffres et des lettres minuscules et majuscules !');
     }
     next();
   }
   catch (error) {
-    res.status(401).json({ error: 'Le mot de passe doit contenir 8 caractères, des chiffres et des lettres minuscules et majuscules !' });
+    res.status(401).json({ error: 'Le mot de passe doit contenir au moins 8 caractères, des chiffres et des lettres minuscules et majuscules !' });
   }
 }
 
